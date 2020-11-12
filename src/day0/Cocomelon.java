5ce1a878faba11d4c67ba80da2234927c2b0bec2
Ayah N <ayah.noor0213@gmail.com>
2020-11-12 11:31:43 -0500

    new messages

--- src/day0/Cocomelon.java
+++ src/day0/Cocomelon.java
@@ -1,7 +1,11 @@
 package day0;
 
+import java.sql.SQLOutput;
+
 public class Cocomelon {
     public static void main(String[] args) {
+        System.out.println("Twinkle");
+        System.out.println("BAby bus");
 
     }
 }
